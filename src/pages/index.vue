8fb20caa7d75f504088086defea68d073057f76e
林景德 <1021440224@qq.com>
2022-04-18 21:56:22 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+ 
+<template>
+    <div>{{ userStore.name }}</div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/store/user'
+
+const userStore = useUserStore()
+</script>
